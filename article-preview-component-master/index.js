624009d1bee0c8cdfecca723fa61f0423196caf1
Gift Amachree <111980187+jen67@users.noreply.github.com>
2023-11-01 23:32:42 +0100

    Delete article-preview-component-master directory

--- article-preview-component-master/index.js
+++
@@ -1,9 +0,0 @@
-
-const shareButton = document.querySelector('#share_image');
-const shareBox = document.querySelector('.hide');
-
-shareButton.addEventListener('click', () => {
-  shareBox.classList.toggle('hide');
-})
-
-
