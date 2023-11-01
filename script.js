78adf83bc6f29e1f4749f73b2342067c8686bfaa
jen67 <amakrigift2000@gmail.com>
2023-11-01 22:58:53 +0100

    added more settings

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
-const shareImg = document.querySelector(".share-icon");
+const shareIcons = document.querySelectorAll(".share-icon");
 const toolTip = document.querySelector("#share");
-const hideToolTip = document.querySelector("#share .share-icon");
 
-
-shareImg.addEventListener("click", () => { 
-    toolTip.style.transform = 'translateY(0)'
-    toolTip.style.opacity = '1'
-    toolTip.style.transition = 'all 0.3s ease-in'
+shareIcons.forEach((shareIcon) => {
+  shareIcon.addEventListener("click", () => {
+    toolTip.classList.toggle("visible");
+  });
 });
-
-hideToolTip.addEventListener("click", () => { 
-    toolTip.style.transform = 'translateY(-100%)'
-    toolTip.style.opacity = '0'
-    toolTip.style.transition = 'all 0.3s ease-out'
-});
-
