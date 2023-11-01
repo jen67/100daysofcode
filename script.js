aaeefa119122168568ceb4bfbb7c8af7a0c14773
jen67 <amakrigift2000@gmail.com>
2023-11-01 19:34:48 +0100

    added more functionality

--- script.js
+++ script.js
@@ -6,10 +6,12 @@ const hideToolTip = document.querySelector("#share .share-icon");
 shareImg.addEventListener("click", () => { 
     toolTip.style.transform = 'translateY(0)'
     toolTip.style.opacity = '1'
+    toolTip.style.transition = 'all 0.3s ease-in'
 });
 
 hideToolTip.addEventListener("click", () => { 
     toolTip.style.transform = 'translateY(-100%)'
     toolTip.style.opacity = '0'
+    toolTip.style.transition = 'all 0.3s ease-out'
 });
 
